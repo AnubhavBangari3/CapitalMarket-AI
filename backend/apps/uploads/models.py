98d4c0363f1c490da59fc45e062d31f923ef0fc1
Anubhav Bangari <bangarianubhav@gmail.com>
2026-05-13 14:55:31 +0530

    Uploaded model added

--- backend/apps/uploads/models.py
+++ backend/apps/uploads/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class UploadedSwiftFile(models.Model):
+
+    STATUS_CHOICES = [
+        ("uploaded", "Uploaded"),
+        ("processing", "Processing"),
+        ("parsed", "Parsed"),
+        ("failed", "Failed"),
+    ]
+
+    original_filename = models.CharField(max_length=255)
+
+    file = models.FileField(
+        upload_to="swift_uploads/"
+    )
+
+    file_type = models.CharField(max_length=20)
+
+    file_size = models.PositiveIntegerField()
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default="uploaded"
+    )
+
+    uploaded_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.original_filename
